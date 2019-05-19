e734b3328717eb99074dc7d709b2c9b62f3d5880
saqeeb360 <saqeeb.360@gmail.com>
2019-05-19 18:15:17 +0530

    corrections done

--- Day 1/miniproject.py
+++ Day 1/miniproject.py
@@ -8,12 +8,35 @@ Created on Wed May  8 14:27:18 2019
 from random import randint
 
 
-secret_number=(randint(0,9))
-guess=input("Enter a number = ")
-print("secret number is " + str(secret_number))
-if (secret_number == guess ):
-    print("Winner")
-elif (secret_number-)
-else:
-    print("Loser")
-  
+secret_number=(randint(1,9))
+i=0
+while i<6:
+    flag=True
+    guess=input("Guess a number = ")
+    if not guess:
+        print("Loser")
+        break
+    if guess not in "1234567890":
+        print("Enter a integer\nLoser")
+    else:
+        guess=int(guess)
+        if (secret_number == guess ):
+            print("Winner")
+            break
+        elif (secret_number-guess>4 or secret_number-guess<-4):
+            print("too high")
+        elif (secret_number-guess<2 and secret_number-guess>-2):
+            print("too close")
+        else:
+            print("Wrong")
+        if i==5:
+            print("Loser")
+            print("secret number is " + str(secret_number))
+            break
+        if i<5:
+            play_again=input("play again any or n : ")
+            if play_again=="n":
+                print("Loser")
+                break
+    i=i+1
+    print("-----------------------------\nNumber of tries left "+str(6-i) )
